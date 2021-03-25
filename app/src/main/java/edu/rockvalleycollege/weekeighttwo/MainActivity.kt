10787171e40abe4388c64d188583a77ab8b5eeb3
MWMWMW222 <77510750+MWMWMW222@users.noreply.github.com>
2021-03-25 13:54:11 -0500

    Initial commit

---
+++ app/src/main/java/edu/rockvalleycollege/weekeighttwo/MainActivity.kt
@@ -0,0 +1,51 @@
+package edu.rockvalleycollege.weekeighttwo
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //bind objects
+        val btnGo = findViewById<Button>(R.id.btnPage2)
+        val txtSend = findViewById<EditText>(R.id.txtSend)
+        val txtSend2 = findViewById<EditText>(R.id.txtSend2)
+
+        btnGo.setOnClickListener(View.OnClickListener {
+            //Intent is used to send data between activities
+            val intent = Intent(this, MainActivity2::class.java)
+            //putExtra sets value to name SendStuff (Could be called whatever you want
+            intent.putExtra("SendStuff",txtSend.text.toString())
+            intent.putExtra("SendStuff2",txtSend2.text.toString())
+            //Go to second activity
+            startActivity(intent)
+        })
+
+        //Fire hidekeyboard when user taps outside any text object
+        //Place below code right before last right bracket in function onCreate
+        findViewById<View>(android.R.id.content).setOnTouchListener { _, _ ->
+            hideKeyboard()
+            false
+        }
+    }
+    //function to hide keyboard goes right before the last right bracket of Class MainActivity
+    //should auto import android.content.Context
+    //should auto add import android.view.inputmethod.InputMethodManager
+    fun hideKeyboard() {
+        try {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        } catch (e: Exception) {
+            // TODO: handle exception
+        }
+    }
+}
+
